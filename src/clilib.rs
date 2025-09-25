3fe36cf2716a6f0f7e8bd0527211214519b58734
Jack Hamilton <jackham800@gmail.com>
2025-09-25 18:38:13 -0500

    Template

--- src/clilib.rs
+++
@@ -1,9 +0,0 @@
-use std::char;
-
-pub struct CLICommand {
-    pub short_flag: char,
-    pub long_flag: String,
-    pub command: fn(),
-    pub description: String
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use crate::clilib::CLICommand;
+use cli_builder_macros::cli_builder;
 use std::env;
 use freezable_trait::freezable;
-
-mod clilib;
 
 static PRODUCT_NAME: &str = "product_name";
 
@@ -26,82 +24,17 @@ fn get_config() -> Config {
     toml_configurator::get_config(PRODUCT_NAME.to_string())
 }
 
-struct Runtime {
-    commands: Vec<CLICommand>,
-    default_command: Option<fn()>
-}
-impl Runtime {
-    fn new() -> Self {
-        Self {
-            commands: vec![
-                CLICommand {
-                    short_flag: 'h',
-                    long_flag: "help".into(),
-                    command: help,
-                    description: "Explains available commands.".into()
-                },
-                CLICommand {
-                    short_flag: 'v',
-                    long_flag: "version".into(),
-                    command: version,
-                    description: "Outputs tool version.".into()
-                },
-            ],
-            default_command: Some(help)
-        }
-    }
-
-    fn run(&self, arg: String) {
-        for command in &self.commands {
-            if let Some(long_arg) = arg.strip_prefix("--") {
-                if command.long_flag == long_arg {
-                    (command.command)()
-                }
-            } else if let Some(short_arg) = arg.strip_prefix("-")
-                   && command.short_flag.to_string() == short_arg {
-                (command.command)()
-            }
-        }
-    }
-
-    fn gen_help(&self) -> String {
-        let mut message: String = "Help:\n".into();
-        for command in &self.commands {
-            let helpmsg = format!("\t -{}, --{}: {}\n", command.short_flag, command.long_flag, command.description);
-            message.push_str(&helpmsg);
-        }
-        message
-    }
+cli_builder! {
+    [
+        Command {
+            short_flag: 't',
+            long_flag: "test",
+            command: test_command,
+            description: "Run a test"
+        },
+    ]
 }
 
-fn help() {
-    // I'm not losing sleep over the fact that this will likely be invoked from an existing runtime
-    // object. Mind, I am losing sleep, just for different reasons.
-    let runtime = Runtime::new();
-    println!("{}", runtime.gen_help());
-    std::process::exit(0);
-}
-
-fn version() {
-    println!("{} version {} by Jack Hamilton", PRODUCT_NAME, env!("CARGO_PKG_VERSION"));
-    std::process::exit(0);
-}
-
-fn main() {
-    let runtime = Runtime::new();
-    let args: Vec<String> = env::args().collect();
-    // First arg is junk
-    if args.is_empty() || args.len() == 1 {
-        if let Some(command) = runtime.default_command {
-            command()
-        } else {
-            help()
-        }
-    } else if args.len() > 2 {
-        println!("Too many arguments!");
-        help()
-    } else {
-        let arg = args[1].clone();
-        runtime.run(arg);
-    }
+fn test_command() {
+    println!("This is a test");
 }
